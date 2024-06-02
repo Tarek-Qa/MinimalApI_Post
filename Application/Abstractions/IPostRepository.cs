a7b2892a22eed1ef0e19532d7a4b1fbaf421f297
Tarek Qassoumeh <144264754+Tarek-Qa@users.noreply.github.com>
2024-06-02 17:17:34 +0200

    Add project files.

---
+++ Application/Abstractions/IPostRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstractions
+{
+    public interface IPostRepository
+    {
+        Task <ICollection<Post>> GetAllPosts();
+
+        Task<Post> GetPostById(int PostId);
+        Task<Post> CreatePost(Post ToCreate);
+
+        Task<Post> UpDatePost(string UpdatedContent, int PostId);
+
+        Task<Post> DeletePost(int PostId);
+    }
+}
